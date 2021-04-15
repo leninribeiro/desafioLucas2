e8ee79ed855c80e82f8fb7a9b1ff5f4aeddfde6b
Lenin Quadros <lenin.ribeiroquadros@gmail.com>
2021-04-15 16:18:39 -0300

    make it run

--- src/main/java/com/example/desafioLucas2/controller/GetMealController.java
+++ src/main/java/com/example/desafioLucas2/controller/GetMealController.java
@@ -1,13 +1,16 @@
 package com.example.desafioLucas2.controller;
 
+import com.example.desafioLucas2.model.Meal;
 import com.example.desafioLucas2.model.Response;
 import com.example.desafioLucas2.service.FetchMealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class GetMealController {
 
@@ -15,8 +18,9 @@ public class GetMealController {
     private FetchMealService fetchMealService;
 
     @GetMapping("/meal/{mealName}")
-    public ResponseEntity<Response> getMeal(@PathVariable String mealName) {
+    public ResponseEntity<Meal[]> getMeal(@PathVariable String mealName) {
         Response meals = this.fetchMealService.fetchMeals(mealName);
-        return ResponseEntity.ok(meals);
+        Meal[] mealList = meals.meals;
+        return ResponseEntity.ok(mealList);
     }
 }
--- src/main/java/com/example/desafioLucas2/model/Response.java
+++ src/main/java/com/example/desafioLucas2/model/Response.java
@@ -1,13 +1,12 @@
 package com.example.desafioLucas2.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class Response {
-    private List<Meal> meals;
+    public Meal[] meals;
 
     public Response() {
     }
